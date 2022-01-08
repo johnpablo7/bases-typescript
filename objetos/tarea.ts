f7bd8e9f003394483b8dc255be38420e65e2022b
john <john.davis.2015.03@gmail.com>
2022-01-08 15:52:33 -0500

    Depuración de Errores y el archivo tsconfig.json

--- objetos/tarea.ts
+++ objetos/tarea.ts
@@ -1,77 +1,81 @@
-// Objetos
-type Cars = {
-  carroceria: string;
-  modelo: string;
-  antibalas: boolean;
-  pasajeros: number;
-  disparar?: () => void;
-};
+(() => {
+  // Objetos
+  type Cars = {
+    carroceria: string;
+    modelo: string;
+    antibalas: boolean;
+    pasajeros: number;
+    disparar?: () => void;
+  };
 
-const batimovil: Cars = {
-  carroceria: "Negra",
-  modelo: "6x6",
-  antibalas: true,
-  pasajeros: 4,
-};
+  const batimovil: Cars = {
+    carroceria: "Negra",
+    modelo: "6x6",
+    antibalas: true,
+    pasajeros: 4,
+  };
 
-const bumblebee: Cars = {
-  carroceria: "Amarillo con negro",
-  modelo: "4x2",
-  antibalas: true,
-  pasajeros: 4,
-  disparar() {
-    // El metodo disparar es opcional
-    console.log("Disparando");
-  },
-};
+  const bumblebee: Cars = {
+    carroceria: "Amarillo con negro",
+    modelo: "4x2",
+    antibalas: true,
+    pasajeros: 4,
+    disparar() {
+      // El metodo disparar es opcional
+      console.log("Disparando");
+    },
+  };
 
-// Villanos debe de ser un arreglo de objetos personalizados
-type Villanos = {
-  nombre: string;
-  edad: number;
-  mutante: boolean;
-};
+  // Villanos debe de ser un arreglo de objetos personalizados
+  type Villanos = {
+    nombre: string;
+    edad: number;
+    mutante: boolean;
+  };
 
-const villanos: Villanos[] = [
-  {
-    nombre: "Lex Luthor",
-    edad: 54,
-    mutante: false,
-  },
-  {
-    nombre: "Erik Magnus Lehnsherr",
-    edad: 49,
-    mutante: true,
-  },
-  {
-    nombre: "James Logan",
-    edad: undefined,
-    mutante: true,
-  },
-];
+  const villanos: Villanos[] = [
+    {
+      nombre: "Lex Luthor",
+      edad: 54,
+      mutante: false,
+    },
+    {
+      nombre: "Erik Magnus Lehnsherr",
+      edad: 49,
+      mutante: true,
+    },
+    {
+      nombre: "James Logan",
+      edad: undefined,
+      mutante: true,
+    },
+  ];
 
-// Multiples tipos
-// cree dos tipos, uno para charles y otro para apocalipsis
-type Charles = {
-  poder: string;
-  estatura: number;
-};
-const charles: Charles = {
-  poder: "psiquico",
-  estatura: 1.78,
-};
+  // Multiples tipos
+  // cree dos tipos, uno para charles y otro para apocalipsis
+  type Charles = {
+    poder: string;
+    estatura: number;
+  };
+  const charles: Charles = {
+    poder: "psiquico",
+    estatura: 1.78,
+  };
 
-type Apocalipsis = {
-  lider: boolean;
-  miembros: string[];
-};
-const apocalipsis: Apocalipsis = {
-  lider: true,
-  miembros: ["Magneto", "Tormenta", "Psylocke", "Angel"],
-};
+  type Apocalipsis = {
+    lider: boolean;
+    miembros: string[];
+  };
+  const apocalipsis: Apocalipsis = {
+    lider: true,
+    miembros: ["Magneto", "Tormenta", "Psylocke", "Angel"],
+  };
 
-// Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
-let mystique: Charles | Apocalipsis;
+  console.log(apocalipsis);
 
-mystique = charles;
-mystique = apocalipsis;
+  // Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
+  let mystique: Charles | Apocalipsis;
+
+  mystique = charles;
+  mystique = apocalipsis;
+})();
--- tipos/arrays.ts
+++ tipos/arrays.ts
@@ -1,19 +1,21 @@
 (() => {
-	// const numbers: (string | number | boolean)[] = [
-	// 	1,
-	// 	2,
-	// 	3,
-	// 	4,
-	// 	'5',
-	// 	6,
-	// 	7,
-	// 	8,
-	// 	9,
-	// 	10
-	// ];
-	const numbers: number[] = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+  // const numbers: (string | number | boolean)[] = [
+  // 	1,
+  // 	2,
+  // 	3,
+  // 	4,
+  // 	'5',
+  // 	6,
+  // 	7,
+  // 	8,
+  // 	9,
+  // 	10
+  // ];
+  const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-	const villians = [ 'Omega rojo', 'Dormammu', 'Duende Verde' ];
+  const villians = ["Omega rojo", "Dormammu", "Duende Verde"];
 
-	villians.forEach((v) => console.log(v.toUpperCase()));
+  villians.forEach((v) => console.log(v.toUpperCase()));
+
+  numbers.forEach((v) => console.log(v));
 })();
