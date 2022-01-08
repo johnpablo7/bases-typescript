3d9cb3142d11b6e418893ed75dbcb9323a8eaff3
john <john.davis.2015.03@gmail.com>
2022-01-08 12:57:51 -0500

    Tipos personalizados

--- objetos/type.js
+++ objetos/type.js
@@ -3,11 +3,14 @@
     let flash = {
         name: "Barry Allen",
         age: 24,
-        powers: ["Súper velocidad", "Viajar en el tiempo"],
+        powers: [1, 2],
     };
     let superman = {
         name: "Clark Kent",
         age: 60,
-        powers: ["Súper velocidad"],
+        powers: [1],
+        getName() {
+            return this.name;
+        },
     };
 })();
